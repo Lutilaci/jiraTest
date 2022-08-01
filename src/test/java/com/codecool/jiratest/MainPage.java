988a5cb6cad10b874b3a46e3a2d0bbedecaf102a
Lutilaci <lutilaci@gmail.com>
2022-08-01 10:09:14 +0200

    modify starter kit

--- src/test/java/com/codecool/jiratest/MainPage.java
+++ src/test/java/com/codecool/jiratest/MainPage.java
@@ -10,12 +10,6 @@ public class MainPage {
     @FindBy(css = "a.wt-button_mode_primary")
     public WebElement seeAllToolsButton;
 
-    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
-    public WebElement toolsMenu;
-
-    @FindBy(css = "[data-test='site-header-search-action']")
-    public WebElement searchButton;
-
     public MainPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
--- src/test/java/com/codecool/jiratest/MainPageTest.java
+++ src/test/java/com/codecool/jiratest/MainPageTest.java
@@ -31,29 +31,6 @@ public class MainPageTest {
         driver.quit();
     }
 
-    @Test
-    public void search() {
-        mainPage.searchButton.click();
-
-        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
-        searchField.sendKeys("Selenium");
-
-        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
-        submitButton.click();
-
-        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
-        assertEquals("Selenium", searchPageField.getAttribute("value"));
-    }
-
-    @Test
-    public void toolsMenu() {
-        new Actions(driver)
-                .moveToElement(mainPage.toolsMenu)
-                .perform();
-
-        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='menu-main-popup-content']"));
-        assertTrue(menuPopup.isDisplayed());
-    }
 
     @Test
     public void navigationToAllTools() {
