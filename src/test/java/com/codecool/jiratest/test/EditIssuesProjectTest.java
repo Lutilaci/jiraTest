9e4a5d73c1c705239e839c9762da050184f11abd
gergelybartha <gergely.bartha95@gmail.com>
2022-08-02 11:04:43 +0200

    Edit existing project tests

---
+++ src/test/java/com/codecool/jiratest/test/EditIssuesProjectTest.java
@@ -0,0 +1,84 @@
+package com.codecool.jiratest.test;
+
+import com.codecool.jiratest.page.EditIssuePage;
+import com.codecool.jiratest.page.LoginPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class EditIssuesProjectTest {
+    private WebDriver driver;
+    private EditIssuePage editIssue;
+    private LoginPage loginPage;
+    private WebDriverWait wait;
+
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        editIssue = new EditIssuePage(driver);
+        loginPage = new LoginPage(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void toucansEditableTest(){
+        driver.get("https://jira-auto.codecool.metastage.net/projects/TOUCAN/issues/TOUCAN-1");
+        logIn();
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+        driver.get("https://jira-auto.codecool.metastage.net/projects/TOUCAN/issues/TOUCAN-2");
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+        driver.get("https://jira-auto.codecool.metastage.net/projects/TOUCAN/issues/TOUCAN-3");
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+    }
+
+    @Test
+    public void koalasEditableTest(){
+        driver.get("https://jira-auto.codecool.metastage.net/projects/COALA/issues/COALA-1");
+        logIn();
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+        driver.get("https://jira-auto.codecool.metastage.net/projects/COALA/issues/COALA-2");
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+        driver.get("https://jira-auto.codecool.metastage.net/projects/COALA/issues/COALA-3");
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+    }
+
+    @Test
+    public void jetisEditableTest(){
+        driver.get("https://jira-auto.codecool.metastage.net/projects/JETI/issues/JETI-1");
+        logIn();
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+        driver.get("https://jira-auto.codecool.metastage.net/projects/JETI/issues/JETI-2");
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+        driver.get("https://jira-auto.codecool.metastage.net/projects/JETI/issues/JETI-3");
+        wait.until(ExpectedConditions.elementToBeClickable(
+                editIssue.editButton)).isDisplayed();
+    }
+
+
+    private void logIn(){
+        loginPage.usernameField.sendKeys("automation23");
+        loginPage.passwordField.sendKeys("CCAutoTest19.");
+        loginPage.logInButton.click();
+    }
+}
