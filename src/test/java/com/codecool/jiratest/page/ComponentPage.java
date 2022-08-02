f959814eed1ecf821cf585bcaa7476cbd9b9d52b
Lutilaci <lutilaci@gmail.com>
2022-08-02 20:39:52 +0200

    Component started

--- src/test/java/com/codecool/jiratest/page/ComponentPage.java
+++ src/test/java/com/codecool/jiratest/page/ComponentPage.java
@@ -10,6 +10,18 @@ public class ComponentPage {
     @FindBy (xpath = "//h1[@id='summary-val']")
     public WebElement summaryVal;
 
+    @FindBy (xpath = "//span[contains(.,'Edit')]")
+    public WebElement editButton;
+
+    @FindBy (css = "#components-textarea")
+    public WebElement componentTextArea;
+
+    @FindBy (css = "#edit-issue-submit")
+    public WebElement editIssueSubmitButton;
+
+    @FindBy (css = ".aui-message")
+    public WebElement popupMessage;
+
     public ComponentPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
